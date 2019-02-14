ec510cd9b2110e9f516a9a4d9add0e3b860298a5
Jim Tsai <jim.h.tsai@gmail.com>
2019-02-14 12:15:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -20,8 +20,17 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  var ipaddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(req.headers)
+  var language = req.headers['accept-language'];
+  var software = req.headers['user-agent'];
+  
+  res.json({
+    ipaddress,
+    language,
+    software
+  })
 });
 
 
